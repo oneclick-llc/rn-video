6669c31423cc8ab1a4f1e121f7e053e9fda40c34
sergeymild <sergeymild@yandex.ru>
2023-07-07 12:19:05 +0300

    fix android build

--- android/src/main/java/com/video/VideoViewManager.kt
+++ android/src/main/java/com/video/VideoViewManager.kt
@@ -1,27 +1,12 @@
 package com.video
 
-import android.graphics.Color
 import com.facebook.react.module.annotations.ReactModule
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
-import com.facebook.react.uimanager.ViewManagerDelegate
-import com.facebook.react.uimanager.annotations.ReactProp
-import com.facebook.react.viewmanagers.VideoViewManagerInterface
-import com.facebook.react.viewmanagers.VideoViewManagerDelegate
-import com.facebook.soloader.SoLoader
 
 @ReactModule(name = VideoViewManager.NAME)
-class VideoViewManager : SimpleViewManager<VideoView>(),
-  VideoViewManagerInterface<VideoView> {
-  private val mDelegate: ViewManagerDelegate<VideoView>
+class VideoViewManager : SimpleViewManager<VideoView>() {
 
-  init {
-    mDelegate = VideoViewManagerDelegate(this)
-  }
-
-  override fun getDelegate(): ViewManagerDelegate<VideoView>? {
-    return mDelegate
-  }
 
   override fun getName(): String {
     return NAME
@@ -31,18 +16,7 @@ class VideoViewManager : SimpleViewManager<VideoView>(),
     return VideoView(context)
   }
 
-  @ReactProp(name = "color")
-  override fun setColor(view: VideoView?, color: String?) {
-    view?.setBackgroundColor(Color.parseColor(color))
-  }
-
   companion object {
     const val NAME = "VideoView"
-
-    init {
-      if (BuildConfig.CODEGEN_MODULE_REGISTRATION != null) {
-        SoLoader.loadLibrary(BuildConfig.CODEGEN_MODULE_REGISTRATION)
-      }
-    }
   }
 }
