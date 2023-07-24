fe0e6abd7d8872c79c3d3844a55af712b84c4c68
DmitriyGalyanov <64410709+DmitriyGalyanov@users.noreply.github.com>
2023-07-24 13:51:02 +0300

    add non-iOS Warn

--- src/VideosController.ts
+++ src/VideosController.ts
@@ -1,10 +1,16 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const LookyVideosManager = NativeModules.VideosController;
+
+const IS_IOS = Platform.OS === 'ios';
 
 const _log = (methodName: string, args?: any) =>
   console.log(
     `rn-video.${methodName}(${args ? JSON.stringify(args, undefined, 2) : ''})`
+  );
+const warnAboutNonIosPlatform = () =>
+  console.warn(
+    'trying to call a method from iOS VideosController on a different platform. Consider using Looky Video Manager'
   );
 
 export function getIdForVideo(
@@ -30,11 +36,13 @@ export function getIdForVideoWithoutChannel(
 
 export function playVideo(channel: string, videoId: string) {
   _log(playVideo.name, { channel, videoId });
+  if (!IS_IOS) return warnAboutNonIosPlatform();
   LookyVideosManager.playVideo(channel, videoId);
 }
 
 export function pauseCurrentPlaying() {
   _log(pauseCurrentPlaying.name);
+  if (!IS_IOS) return warnAboutNonIosPlatform();
   LookyVideosManager.pauseCurrentPlaying();
 }
 
@@ -43,6 +51,7 @@ export function togglePlayInBackground(
   playInBackground: boolean
 ) {
   _log(togglePlayInBackground.name, { channelName, playInBackground });
+  if (!IS_IOS) return warnAboutNonIosPlatform();
   LookyVideosManager.togglePlayInBackground(channelName, playInBackground);
 }
 
@@ -50,6 +59,7 @@ export function pauseCurrentPlayingWithLaterRestore(
   channelName: string | undefined
 ) {
   _log(pauseCurrentPlayingWithLaterRestore.name, { channelName });
+  if (!IS_IOS) return warnAboutNonIosPlatform();
   LookyVideosManager.pauseCurrentPlayingWithLaterRestore(channelName);
 }
 
@@ -58,16 +68,19 @@ export function restoreLastPlaying(
   shouldSeekToStart: boolean = true
 ) {
   _log(restoreLastPlaying.name, { channelName, shouldSeekToStart });
+  if (!IS_IOS) return warnAboutNonIosPlatform();
   LookyVideosManager.restoreLastPlaying(channelName, shouldSeekToStart);
 }
 
 export function togglePlayVideo(channel: string, videoId: string) {
   _log(togglePlayVideo.name, { channel, videoId });
+  if (!IS_IOS) return warnAboutNonIosPlatform();
   LookyVideosManager.togglePlay(channel, videoId);
 }
 
 export function toggleVideosMuted(muted: boolean) {
   _log(toggleVideosMuted.name, { muted });
+  if (!IS_IOS) return warnAboutNonIosPlatform();
   LookyVideosManager.toggleVideosMuted(muted);
 }
 
@@ -75,5 +88,6 @@ export function toggleVideosMutedEvent(event: {
   nativeEvent: { muted: boolean };
 }) {
   _log(toggleVideosMutedEvent.name, { event });
+  if (!IS_IOS) return warnAboutNonIosPlatform();
   LookyVideosManager.toggleVideosMuted(event.nativeEvent.muted);
 }
